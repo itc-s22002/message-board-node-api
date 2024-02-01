a4f0a16a86dcaba1580710da261c5f4f0d189e43
s22002 <s22002@std.it-college.ac.jp>
2024-02-01 15:25:40 +0900

    ログインまわり完了

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import session from "express-session";
+import passport from "passport";
+import passportConfig from "./util/auth.js";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
@@ -13,6 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(import.meta.dirname, "public")));
+
+// session
+app.use(session({
+    secret: "Ym6JrhVjwB4IB&ivXWr9+&xw",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60 * 60 * 1000}
+}));
+// passport
+app.use(passport.authenticate("session"));
+app.use(passportConfig(passport));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,79 @@
 import express from "express";
+import passport from "passport";
+import {check, validationResult} from "express-validator";
+import {calcHash, generateSalt} from "../util/auth.js";
+import {PrismaClient} from "@prisma/client";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*
+* 状態確認
+*/
+router.get('/', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.status(200).json({ message: "logged in"});
+    } else {
+        res.status(401).json({ message: 'unauthenticated' });
+    }});
+/*
+* ログイン
+*/
+
+// router.post("/login", passport.authenticate("local", {
+//     successReturnToOrRedirect: "/",
+//     failureRedirect: "/users/login",
+//     failureMessage: true,
+//     keepSessionInfo: true
+// }));
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) { return next(err); }
+        if (!user) { return res.status(401).json({ message: "name and/or password is invalid" }); }
+
+        req.logIn(user, (err) => {
+            if (err) { return next(err); }
+            return res.status(200).json({ message: "OK"});
+        });
+    })(req, res, next);
 });
 
+/*
+* 新規登録
+*/
+
+router.post("/signup", async (req, res, next) => {
+    try {
+        const { name , pass } = req.body;
+        if (!name) {
+            return res.status(400).json({message: "not name"});
+        }
+        const salt = generateSalt();
+        const hashedPassword = calcHash(pass, salt);
+        await prisma.user.create({
+            data: {
+                name,
+                pass: hashedPassword,
+                salt
+            }
+        });
+        res.status(200).json({message: "created!"});
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+/*
+* ログアウト
+*/
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+    });
+    res.status(200).json({message: "OK"});
+});
 export default router;
